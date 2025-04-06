530c5a7ad4a78d68934c4af778ce1e7ead41e2cd
Joelle <joelle.marianek@live.de>
2025-04-06 22:36:05 +0200

    fix: remove hardcoded array

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,15 @@ export default {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {extend: {}},
+  theme: {extend: {
+    colors: {
+      blush: '#D88A95',
+      lilac: '#C3A6CC',
+      apricot: '#E8BA8C',
+      sage: '#A9C3A5',
+      charcoal: '#2E2E30',
+      cream: '#FAF8F5',
+    }
+  }},
   plugins: [],
 }
